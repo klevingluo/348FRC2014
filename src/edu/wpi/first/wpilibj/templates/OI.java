78c29cba7b5629d3b78b07e201108c685b89dbc2
klevin.gluo <klevin.gluo@gmail.com>
2014-02-12 08:42:20 -0500

    remapped controls in a better way

--- src/edu/wpi/first/wpilibj/templates/OI.java
+++ src/edu/wpi/first/wpilibj/templates/OI.java
@@ -16,35 +16,28 @@ public class OI {
     public Joystick rightStick = new Joystick(2);
     
     
-    // when pressed, the robot drives backwards
+    // while pressed, the robot drives backwards
     public JoystickButton reverseDrive = new JoystickButton(leftStick, 1);
-    // rolls the intake rollers 
-    public JoystickButton roll = new JoystickButton(leftStick, 5);
-    // rolls the intake rollers backwards
-    public JoystickButton rollReverse = new JoystickButton(leftStick, 4);
+    // rolls the intake rollers to take in a ball
+    public JoystickButton roll = new JoystickButton(leftStick, 3);
+    // rolls the intake rollers backwards to spit out a ball
+    public JoystickButton rollReverse = new JoystickButton(leftStick, 2);
     // raises the arms
-    public JoystickButton raise = new JoystickButton(leftStick, 3);
+    public JoystickButton raise = new JoystickButton(rightStick, 3);
     // lowers the arms
-    public JoystickButton lower = new JoystickButton(leftStick, 2);
+    public JoystickButton lower = new JoystickButton(rightStick, 2);
+    
+    //moves to the next stage of shooting
+    public JoystickButton advance = new JoystickButton(leftStick, 5);
+    //moves to the previous state of shooting
+    public JoystickButton abort = new JoystickButton(leftStick, 4);
+    
     // turns the compressor on/off
     public JoystickButton toggle = new JoystickButton(rightStick, 10);
-    // enters normal controll mode
-    public JoystickButton normalControl = new JoystickButton(leftStick, 6);
-    // enters autonomous test mode
-    public JoystickButton test = new JoystickButton(leftStick,7);
-    // enters launcher diagnostic mode
-    public JoystickButton shooterTest = new JoystickButton(leftStick, 10);
-    
-    public JoystickButton advance = new JoystickButton(rightStick, 3);
-    public JoystickButton abort = new JoystickButton(rightStick, 2);
-    
     public void run() {
-        normalControl.whenPressed(new teleopDefault());
         toggle.whenReleased(new ToggleCompressor());
         reverseDrive.whenActive(new ReverseTankDrive());
         reverseDrive.whenInactive(new TankDrive());
-        
-        
     }
 }
 
